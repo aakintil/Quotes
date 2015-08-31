5ccfa4836544a76f98001b96b7c41f40409eaca8
Aderinsola <derin_akintilo@ziba.com>
2015-08-31 14:37:22 -0700

    time for designing

--- templates/pages/home/home.js
+++ templates/pages/home/home.js
@@ -3,15 +3,43 @@
     -------- Home JS --------
     -------------------------
 */
+//_self = function( that ) {
+//    return that; 
+//}; 
 
 
-if (Meteor.isClient) {
+if ( Meteor.isClient ) {
     // counter starts at 0
-    Session.setDefault('counter', 0);
+    Session.setDefault( 'counter', 0 );
+    checked = false; 
+    Template.home.rendered = function() {
+
+    }
+
+    function init_quotes() {
+        rand = Math.floor( Math.random() * quotes.length + 1 );
+    }
+
+    Template.home.created = function() {
+        var rand = "", 
+            self = this;
+        Session.set( 'quote', quotes[ 0 ] ); 
+    }
+
+    // working
+    //    Meteor.setInterval( function() {
+    //        var rand = Math.floor( Math.random() * quotes.length + 1 ), 
+    //            quote = quotes[ rand ]; 
+    //        Session.set( 'quote', quote ); 
+    //    }, 10000 )
+
 
     Template.home.helpers({
         counter: function () {
             return Session.get('counter');
+        }, 
+        quote: function() {
+            return Session.get( 'quote' ); 
         }
     });
 
@@ -23,8 +51,17 @@ if (Meteor.isClient) {
     });
 }
 
-if (Meteor.isServer) {
+if ( Meteor.isServer ) {
     Meteor.startup(function () {
         // code to run on server at startup
     });
+}
+
+
+self = function( template ) {
+    this.template = template; 
+}
+
+get_self = function() {
+    return self(); 
 }
--- templates/pages/main/main.js
+++ templates/pages/main/main.js
@@ -4,6 +4,94 @@
     --------------------------------------
 */
 
+quotes = [
+    {
+        quote: "i'm an asshole when i'm walking...i don't give a shit!", 
+        author: "lauren steinberg"
+    }, 
+    {
+        quote: "if you don't feel safe there...then it's not home.", 
+        author: "patt warden"
+    }, 
+    {
+        quote: "a victim?!...i'm her fucking slave man...it's a curse...and a blessing...", 
+        author: "brendan stripling"
+    }, 
+    {
+        quote: "macklemore is the bomob.com", 
+        author: "aviva"
+    }, 
+    {
+        quote: "halal in the streets, haram in the sheets", 
+        author: "Akimuno | Reddit", 
+        context: 'in response to Israeli Palestinian comments'
+    }, 
+    {
+        quote: "no one can ever go back and start a new beginning, but anyone can start today and make a new ending", 
+        author: "kare"
+    }, 
+    {
+        quote: "i don't feel anything when i listen to r&b...it makes me feel empty", 
+        author: "melanie baczynski"
+    }, 
+    {
+        quote: "you love everything that we produce but you don't love the people who produce it", 
+        author: "malcolm", 
+        context: "black lives matter"
+    }, 
+    {
+        quote: "i'm so ridiculous...i'm ridonkculous: true true!", 
+        author: "joanna gallacinao"
+    }, 
+    {
+        quote: "life can be a mean drunk sometimes...never knows when to stop punching", 
+        author: "agent cole", 
+        context: "marvel's agents of shield"
+    }, 
+    {
+        quote: "i'm an asshole when i'm walking...i don't give a shit!", 
+        author: "Lauren Steinberg"
+    }, 
+    {
+        quote: "when michael jackson died, it was like...fuck...when oprah dies, it's gonna be like fuuuuuuuuuck", 
+        author: "joanna gallacinao"
+    }, 
+    {
+        quote: "sometimes you gotta punch, even if you're not in the mood to punch", 
+        author: "derin akintilo"
+    }, 
+    {
+        quote: "i just want hot girls to start conversations with me, instead of ugly girls telling me i got beautiful teeth", 
+        author: "wale desalu"
+    }, 
+    {
+        quote: "we white people...we still run these streets", 
+        author: "jacob johnson", 
+        context: 'discussing racial issues and police brutality'
+    },         
+    {
+        quote: "if it's the world cup, then like...fuck", 
+        author: "joanna gallacinao"
+    },
+    {
+        quote: "my ass is outta control in these pants", 
+        author: "zara friedland"
+    }, 
+    {
+        quote: "it doesn't matter who you marry, because you can always love whoever you want", 
+        author: "sara anya's baboochka"
+    }, 
+    {
+        quote: "his sock game, is on feet", 
+        author: "wale desalu"
+    }, 
+    {
+        quote: "hi waisted shorts have been ruining asses for years. Some bullshit", 
+        author: "corey barbusio"
+    }
+]; 
+
+
 if ( Meteor.isClient ) { 
 
 }
